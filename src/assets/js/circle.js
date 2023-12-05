a5c782e91ac2e039bbacecfef36d02045012a818
L-jy16 <89904583+uUZINN@users.noreply.github.com>
2023-12-05 19:06:47 +0900

    2023.12.05

--- src/assets/js/circle.js
+++ src/assets/js/circle.js
@@ -10,26 +10,21 @@ export function circle() {
     const ani4 = gsap.timeline();
     ani4.to(".circle .c2, .circle .c4, .circle .c6", { rotation: -180 });
 
-
     const masterTimeline = gsap.timeline();
     masterTimeline.add(ani3, 0).add(ani4, 0);
 
     ScrollTrigger.create({
         animation: masterTimeline,
         trigger: ".circle",
-        start: "center center", // 시작 시점
-        end: "+=5500", // 시작 지점에서 4000px 스크롤된 지점에서 종료
+        start: "+=2000 center",
+        //end: "+=5500",
         scrub: true,
-        markers: true,
         pin: true,
         anticipatePin: 1,
         endTrigger: ".end-trigger",
         duration: 10,
-        onEnter: () => {
-            document.querySelector('.circle').classList.remove('end-style');
-        },
-        onLeaveBack: () => {
-            document.querySelector('.circle').classList.add('end-style');
+        onComplete: () => {
+            document.querySelector('.circle').style.position = 'sticky !important';
         }
     });
 }
