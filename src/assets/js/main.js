4b5c1223d63431efac27304aee656a8a522c6087
L-jy16 <89904583+uUZINN@users.noreply.github.com>
2023-12-06 12:55:42 +0900

    2023.12.06

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,12 @@
 import { work } from "./work.js";
 import { smooth } from "./smooth.js";
 import { circle } from "./circle.js";
+import { setFlowBanner } from "./setFlowBanner.js";
 
 
 window.addEventListener("load", function () {
     circle();
     work();
     smooth();
+    setFlowBanner();
 })
